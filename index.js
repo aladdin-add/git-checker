afb364142fb5ed0f39e5ce33f6e153ac873914c1
薛定谔的猫 <zhaoshengdong@meituan.com>
2017-05-12 00:15:27 +0800

    检查是否merge过test

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function () {
-  return true
+var shell = require('shelljs')
+
+module.exports.hasMergedTest = function (msg) {
+  var reg = /merge.*from (test|staging)\w* to (develop|master)/i
+  msg = msg || shell.exec('git log --merges')
+  var mergeTestMsg = reg.exec(msg)
+  if (mergeTestMsg) {
+    console.log('您的分支似乎merge过' + mergeTestMsg[1] + '：')
+    console.log(mergeTestMsg[0])
+    return true
+  }
+  return false
 }
--- test.js
+++ test.js
@@ -1,7 +1,35 @@
-var checkBranch = require('./index.js')
-const assert = require('assert')
-const tman = require('tman')
+var hasMergedTest = require('./index.js').hasMergedTest
+var assert = require('assert')
+var tman = require('tman')
+var mergedMsg = ''
 
-tman.it('exports', function () {
-  assert.strictEqual(checkBranch(), true)
+tman.it('hasMergedTest', function () {
+  mergedMsg = 'Merge pull request #772 in HFE/xx from test to develop'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+  mergedMsg = 'Merge pull request #772 in HFE/xx from test01 to develop'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from staging to develop'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from staging01 to develop'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from test to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from test01 to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from staging to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from staging01 to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), true)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from feature-xx to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), false)
+
+  mergedMsg = 'Merge pull request #772 in HFE/xx from 1232423423 to master'
+  assert.strictEqual(hasMergedTest(mergedMsg), false)
 })
